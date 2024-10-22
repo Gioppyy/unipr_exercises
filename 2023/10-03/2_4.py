10222529be933e44e3d52d35c04e46800c45ef9e
Gioppyy <Giopanno006.com@gmail.com>
2024-10-22 11:31:17 +0200

    .

--- 2023/10-03/2_4.py
+++ 2023/10-03/2_4.py
@@ -1,1 +1,1 @@
-print("\n".join(" ".join(f"{(x**2) + y:2}" for x in range(5)) for y in range(5)))
+print("\n".join(" ".join(f"{abs(x-y):2}" for x in range(5)) for y in range(5)))
--- 2024/10-07/2_8.py
+++ 2024/10-07/2_8.py
@@ -1,5 +1,5 @@
 def check_chars(s: str):
-    z, u = 0
+    z, u = 0, 0
 
     for c in s: 
         if c == "0": z += 1
@@ -7,14 +7,31 @@ def check_chars(s: str):
 
     return (z, u)
 
+"""
+Se volessimo contare gli 0 ed 1 dei bit in una frase
+    def check_chars(s: str):
+        z, u = 0, 0
+
+        for c in s: 
+            c = bin(ord(c))[2::]
+            for b in c:
+                if b == "0": z += 1
+                elif b == "1": u += 1
+
+        return (z, u)
+"""
+
+"""
+Volendo possiamo usare le funzioni di python
+    def check_chars(s: str):
+        return (s.count("0"), s.count("1"))
+"""
+
 def main():
-    s = "a"
-    while s != "":
-        s = input("Inserisci una frase di cui vuoi sapere il numero di 0 e 1 (ENTER for exit): ")
-        if s != "":
-            res = check_chars(s)
-            print(f"Numero di 0: {res[0]}")
-            print(f"Numero di 1: {res[1]}")
+    while ((s := input("Inserisci un testo di cui contare gli 0 e gli 1 (ENTER for exit): ")) != ""):
+        res = check_chars(s)
+        print(f"Numero di 0: {res[0]}")
+        print(f"Numero di 1: {res[1]}")
 
 if __name__ == "__main__":
     main()
